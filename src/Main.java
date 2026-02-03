6abecd2f0489a9e84306758816b72a81ea36aba2
Viktor <viktor4strlsov@gmail.com>
2026-02-04 00:42:47 +0600

    new projekt

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! my name is oleeeg");
+
+        int age;
+        double price;
+        boolean isAdult;
+        char initial;
+
+        age = 30;
+        price = 19.99;
+        isAdult = true;
+        initial = 'K';
+
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isAdult);
+        System.out.println(initial);
+    }
+}
